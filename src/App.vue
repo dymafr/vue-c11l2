d7ed8e9cced6242d095cabb246243a0e3685fa80
dymafr <erwan1309@hotmail.com>
2022-03-14 16:20:21 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <input v-model="usernameValue" type="text" />
+  <p v-if="usernameError">{{ usernameError }}</p>
 
   <pre>
     {{ usernameValue }}
@@ -7,9 +8,24 @@
 </template>
 
 <script setup lang="ts">
-import { useField } from 'vue-validate';
+import { useField } from 'vee-validate';
 
-const { value: usernameValue } = useField('username');
+function validateName(value: string): true | string {
+  if (!value.length) {
+    return 'Le champ est obligatoire';
+  } else if (value.length < 3) {
+    return 'Le champ est trop court';
+  } else if (value.length > 10) {
+    return 'Le champ est trop long';
+  } else {
+    return true;
+  }
+}
+
+const { value: usernameValue, errorMessage: usernameError } = useField(
+  'username',
+  validateName
+);
 </script>
 
 <style scoped lang="scss"></style>

e39ca9a36357fbd1ebf5b43cb1d52793b428d61a
dymafr <erwan1309@hotmail.com>
2022-03-14 16:22:57 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -12,25 +12,22 @@ import { useField, useForm } from 'vee-validate';
 
 const validationSchema = {
   username(value: string): true | string {
-  if (!value.length) {
-    return 'Le champ est obligatoire';
-  } else if (value.length < 3) {
-    return 'Le champ est trop court';
-  } else if (value.length > 10) {
-    return 'Le champ est trop long';
-  } else {
-    return true;
-  }
+    if (!value.length) {
+      return 'Le champ est obligatoire';
+    } else if (value.length < 3) {
+      return 'Le champ est trop court';
+    } else if (value.length > 10) {
+      return 'Le champ est trop long';
+    } else {
+      return true;
+    }
+  },
+};
 
-  }
-}
+useForm({ validationSchema });
 
-function validateName
-
-const { value: usernameValue, errorMessage: usernameError } = useField(
-  'username',
-  validationSchema
-);
+const { value: usernameValue, errorMessage: usernameError } =
+  useField('username');
 </script>
 
 <style scoped lang="scss"></style>
